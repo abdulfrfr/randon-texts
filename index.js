11f4324e107b8be3b8b387efc912a432d0d7ebb4
abdulfrfr <abdulmails112@gmail.com>
2022-08-10 06:49:19 +0100

    'rand'

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ function makeid(length) {
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() *
- charactersLength));
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
    }
    return result;
 }
 
-console.log(makeid(8));
+console.log(makeid(6));
